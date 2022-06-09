064bae891b2012039453dd09e68924519c5d3cda
SantiagoVinicius <viniciussantiagosantos19@gmail.com>
2022-06-09 19:56:47 -0300

    AppGorjeta

---
+++ AppGorjeta/www/js/funcoes.js
@@ -0,0 +1,8 @@
+function CalcularGorjeta(){
+    let valorTotal = document.querySelector("#valor").value
+    valorGorjeta = valorTotal * 0.1
+    document.querySelector("#resultado").innerHTML = "Gorjeta: R$ " + valorGorjeta
+}
+document.querySelector("#btCalcular").addEventListener("click", function(){
+    CalcularGorjeta()
+})
